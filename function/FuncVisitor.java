9306ffb92ca6148df1dde271c388c139dcb8efe9
Paulolrl <39174863+Paulolrl@users.noreply.github.com>
2019-10-13 01:21:44 -0300

    visit do function e correções

--- function/FuncVisitor.java
+++ function/FuncVisitor.java
@@ -1,196 +1,229 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class FuncVisitor extends FunctionsBaseVisitor<Integer> {
+public class FuncVisitor extends FunctionsBaseVisitor < Integer > {
 
-	private HashMap<String, Integer> funcs = new HashMap<String, Integer>(); //guarda função e quantidade de parametros
-	private ArrayList<String> var = new ArrayList<String>(); // guarda variáveis
-	private ArrayList<String> param = new ArrayList<String>(); // guarda parametros da função, para verificar se as variáveis estão no escopo dela
-	private int lastRegister = 0;
-	private String code = "";
-	private String init = "init {\n";
-	private ArrayList<Integer>  params =  new ArrayList<Integer>();
-	private boolean isCode = true;
-
-
-	@Override
-	public Integer visitProgram(FunctionsParser.ProgramContext ctx) {
-		visit(ctx.root());
-		init += "}";
-		System.out.println("code" + code);
-		System.out.println(init);
-		// File file = new File("code.ll"); // Specify the filename
-		return 0;
-	}
-  @Override
-  public Integer visitDecVar(FunctionsParser.DecVarContext ctx) {
-  	// if(!var.contains(ctx.ID().getText()) && funcs.get(ctx.ID().getText()) == null) {
-  	// 	var.add(ctx.ID().getText());
-  	// }else {
-  	// 	//ERRO já foi declarado
-  	// 	System.out.println("Symbol already declared: " + ctx.ID().getText());
-  	// }
-		// visit(ctx.expr());
-		// return 0;
-		code += "@" + ctx.ID().getText() + " = global i32 0\n";
-		isCode = false;
-		int valuereg = visit(ctx.expr());
-		// init += ctx.ID().getText() + "= " + String.valueOf(valuereg);
-		isCode = true;
-
-		return 0;
-  }
-
-	public Integer visitDecFunc(FunctionsParser.DecFuncContext ctx){
-		code += "define i32 @" + ctx.ID().getText() + "(";
-		param.clear();
-		visit(ctx.ids());
-		System.out.println(param);
-		for(int i=0; i<param.size()-1; i++){
-			code += "i32 " + param.get(i) + ", ";
-		}
-		code += "i32 " + param.get(param.size()-1) + "){\n";
-		// if(!var.contains(ctx.ID().getText()) && funcs.get(ctx.ID().getText()) == null) {
-		// 	param.clear();
-		// 	int n = visit(ctx.ids()); //guardar funções no hash com quantidade de parametros
-		// 	funcs.put(ctx.ID().getText(), n);
-		// 	visit(ctx.expr());
-		// 	param.clear();
-		// }else{
-		// 	System.out.println("Symbol already declared: " + ctx.ID().getText());
-		// }
-		// visit(ctx.ids());
-		isCode = true;
-		visit(ctx.expr());
-		code += "	ret %" + lastRegister + "\n" + "}\n";
-		return 0;
-	}
-
-	public Integer visitParamIds(FunctionsParser.ParamIdsContext ctx){
-		return visit(ctx.ids(0)) + visit(ctx.ids(1));
-	}
-
-	public Integer visitParamId(FunctionsParser.ParamIdContext ctx){
-		param.add("%"+ctx.ID().getText());
-		return 1;
-	}
-
-  public Integer visitFunction(FunctionsParser.FunctionContext ctx) {
-		params = new ArrayList<Integer>();
-		visit(ctx.values());
-		// code += 'call f' (kdjafkjdslajflasdjflkasd);
-		// res
-		return 0;
-  }
+    private HashMap < String,
+    Integer > funcs = new HashMap < String,
+    Integer > (); //guarda função e quantidade de parametros
+    private ArrayList < String >
+    var = new ArrayList < String > (); // guarda variáveis
+    private ArrayList < String > param = new ArrayList < String > (); // guarda parametros da função, para verificar se as variáveis estão no escopo dela
+    private int lastRegister = 0;
+    private String code = "";
+    private String init = "deﬁne void @init()\n";
+    private ArrayList < Integer > args = new ArrayList < Integer > ();
+    private boolean isCode = true;
 
 
-  public Integer visitExprSoma(FunctionsParser.ExprSomaContext ctx){
-  	int leftreg = visit(ctx.expr());
-  	int rightreg = visit(ctx.muldiv());
-		int resreg = ++lastRegister;
-		String instr = "	%" + resreg + " = " + "add i32 "+ leftreg + ", " + rightreg + "\n";
-		if(isCode){
-			code += instr;
-		}else{
-			init += instr;
-		}
-		return resreg;
-  } //visit children vai descer na árvore visitando os filhos
+    @Override
+    public Integer visitProgram(FunctionsParser.ProgramContext ctx) {
+        visit(ctx.root());
+        init += "ret void \n}";
+        System.out.print("code: \n" + code);
+        System.out.println("init: \n" + init);
+        // File file = new File("code.ll"); // Specify the filename
+        return 0;
+    }
+    @Override
+    public Integer visitDecVar(FunctionsParser.DecVarContext ctx) {
+        // if(!var.contains(ctx.ID().getText()) && funcs.get(ctx.ID().getText()) == null) {
+        // 	var.add(ctx.ID().getText());
+        // }else {
+        // 	//ERRO já foi declarado
+        // 	System.out.println("Symbol already declared: " + ctx.ID().getText());
+        // }
+        // visit(ctx.expr());
+        // return 0;
+        code += "@" + ctx.ID().getText() + " = global i32 0\n";
+        isCode = false;
+        int valuereg = visit(ctx.expr());
+        init += "store i32 %" + valuereg + "i32* @" + ctx.ID().getText() + "\n";
+        isCode = true;
+        return 0;
+    }
 
-	public Integer visitExprSub(FunctionsParser.ExprSubContext ctx){
-		int leftreg = visit(ctx.expr());
-  	int rightreg = visit(ctx.muldiv());
-		int resreg = ++lastRegister;
-		String instr = "	%" + resreg + " = " + "sub i32 "+ leftreg + ", " + rightreg + "\n";
-		if(isCode){
-			code += instr;
-		}else{
-			init += instr;
-		}
-  	return resreg;
-	} //visit children vai descer na árvore visitando os filhos
+    public Integer visitDecFunc(FunctionsParser.DecFuncContext ctx) {
+        code += "define i32 @" + ctx.ID().getText() + "(";
+        param.clear();
+        visit(ctx.ids());
+        System.out.println(param);
+        for (int i = 0; i < param.size() - 1; i++) {
+            code += "i32 %" + param.get(i) + ", ";
+        }
+        code += "i32 %" + param.get(param.size() - 1) + "){\n";
+        // if(!var.contains(ctx.ID().getText()) && funcs.get(ctx.ID().getText()) == null) {
+        // 	param.clear();
+        // 	int n = visit(ctx.ids()); //guardar funções no hash com quantidade de parametros
+        // 	funcs.put(ctx.ID().getText(), n);
+        // 	visit(ctx.expr());
+        // 	param.clear();
+        // }else{
+        // 	System.out.println("Symbol already declared: " + ctx.ID().getText());
+        // }
+        // visit(ctx.ids());
+        isCode = true;
+        int resreg = visit(ctx.expr());
+        code += "	ret %" + resreg + "\n" + "}\n";
+        return 0;
+    }
 
-	public Integer visitExprMuldiv(FunctionsParser.ExprMuldivContext ctx){
-		return visit(ctx.muldiv());
-	} //visit children vai descer na árvore visitando os filhos
+    public Integer visitParamIds(FunctionsParser.ParamIdsContext ctx) {
+        return visit(ctx.ids(0)) + visit(ctx.ids(1));
+    }
 
-	public Integer visitMuldivMul(FunctionsParser.MuldivMulContext ctx){
-		int leftreg = visit(ctx.muldiv());
-  	int rightreg = visit(ctx.paren());
-		int resreg = ++lastRegister;
-		String instr = "	%" + resreg + " = " + "mul i32 %"+ leftreg + ", %" + rightreg + "\n";
-		if(isCode){
-			code += instr;
-		}else{
-			init += instr;
-		}
-  	return resreg;
-	} //visit children vai descer na árvore visitando os filhos
+    public Integer visitParamId(FunctionsParser.ParamIdContext ctx) {
+        param.add(ctx.ID().getText());
+        return 1;
+    }
 
-	public Integer visitMuldivDiv(FunctionsParser.MuldivDivContext ctx){
-    int leftreg = visit(ctx.muldiv());
-		int rightreg = visit(ctx.paren());
-		int resreg = ++lastRegister;
-		String instr = "	%" + resreg + " = " + "div i32 %"+ leftreg + ", %" + rightreg + "\n";
-		if(isCode){
-			code += instr;
-		}else{
-			init += instr;
-		}
-  	return resreg;
-	} //visit children vai descer na árvore visitando os filhos
+    public Integer visitFunction(FunctionsParser.FunctionContext ctx) {
+        int resreg = ++lastRegister;
+        String instr = "%" + resreg + " = call i32 @" + ctx.ID + "(i32 "%x)
+        args.clear();
+        visit(ctx.values());
+        for(int i = 0; i < args.size(); i++){
+            if(i != args.size() -1){
+                instr += "%" + args.get(i) + ", ";
+            }else{
+                instr += "%" + args.get(i) + ")\n";
+            }
+        }
+        if(isCode){
+            code += instr;
+        }else{
+            init += instr;
+        }
 
-	public Integer visitMuldivParen(FunctionsParser.MuldivParenContext ctx){
-		return visit(ctx.paren());
-	} //visit children vai descer na árvore visitando os filhos
+        return resreg;
+    }
 
-	public Integer visitParenID(FunctionsParser.ParenIDContext ctx){
-		int resreg = ++lastRegister;
-		if(param.contains("%"+ctx.ID().getText())){
-			String instr = "	store i32 " + "%" + ctx.ID().getText() + ", i32* %" + resreg + "\n";
-		}else{
-			String instr = "	store i32 " + "@" + ctx.ID().getText() + ", i32* %" + resreg + "\n";
-		}
-		if(isCode){
-			code += instr;
-		}else{
-			init += instr;
-		}
-		return resreg;
-	}
 
-	public Integer visitParenFunc(FunctionsParser.ParenFuncContext ctx) {
-		return visit(ctx.func());
-	}
+    public Integer visitExprSoma(FunctionsParser.ExprSomaContext ctx) {
+        int leftreg = visit(ctx.expr());
+        int rightreg = visit(ctx.muldiv());
+        int resreg = ++lastRegister;
+        String instr = "	%" + resreg + " = " + "add i32 " + leftreg + ", " + rightreg + "\n";
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return resreg;
+    } //visit children vai descer na árvore visitando os filhos
 
-	public Integer visitParenNum(FunctionsParser.ParenNumContext ctx){
-		int resreg = ++lastRegister;
-		String instr = "	store i32 " + ctx.NUM().getText() + ", i32* %" + resreg + "\n";
-		if(isCode){
-			code += instr;
-		}else{
-			init += instr;
-		}
-		return resreg;
-	}
+    public Integer visitExprSub(FunctionsParser.ExprSubContext ctx) {
+        int leftreg = visit(ctx.expr());
+        int rightreg = visit(ctx.muldiv());
+        int resreg = ++lastRegister;
+        String instr = "	%" + resreg + " = " + "sub i32 " + leftreg + ", " + rightreg + "\n";
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return resreg;
+    } //visit children vai descer na árvore visitando os filhos
 
-	public Integer visitArgvNum(FunctionsParser.ArgvNumContext ctx) {
-		if(isCode)
-			params.add(Integer.parseInt(ctx.NUM().getText()));
-		return 1;
-	}
+    public Integer visitExprMuldiv(FunctionsParser.ExprMuldivContext ctx) {
+        return visit(ctx.muldiv());
+    } //visit children vai descer na árvore visitando os filhos
 
-	public Integer visitArgvId(FunctionsParser.ArgvIdContext ctx) {
-		String id = ctx.ID().getText();
-		if(!param.contains(id) && !var.contains(id)) {
-			//ERRO argumento inválido
-			System.out.println("Bad used symbol: " + ctx.ID().getText());
-		}
-		return 1;
-	}
+    public Integer visitMuldivMul(FunctionsParser.MuldivMulContext ctx) {
+        int leftreg = visit(ctx.muldiv());
+        int rightreg = visit(ctx.paren());
+        int resreg = ++lastRegister;
+        String instr = "	%" + resreg + " = " + "mul i32 %" + leftreg + ", %" + rightreg + "\n";
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return resreg;
+    } //visit children vai descer na árvore visitando os filhos
 
-	public Integer visitArgvFunc(FunctionsParser.ArgvFuncContext ctx) {
-		return visit(ctx.values(0)) + visit(ctx.values(1));
-	}
+    public Integer visitMuldivDiv(FunctionsParser.MuldivDivContext ctx) {
+        int leftreg = visit(ctx.muldiv());
+        int rightreg = visit(ctx.paren());
+        int resreg = ++lastRegister;
+        String instr = "	%" + resreg + " = " + "div i32 %" + leftreg + ", %" + rightreg + "\n";
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return resreg;
+    } //visit children vai descer na árvore visitando os filhos
 
+    public Integer visitMuldivParen(FunctionsParser.MuldivParenContext ctx) {
+        return visit(ctx.paren());
+    } //visit children vai descer na árvore visitando os filhos
+
+    public Integer visitParenID(FunctionsParser.ParenIDContext ctx) {
+        int resreg = ++lastRegister;
+        String instr = "%" + resreg + " = ";
+        if (param.contains(id)) {
+            instr += "add i32 %" + id + ", 0\n";
+        }
+        if (var.contains(id)) {
+            instr += "load i32, i32* @" + id + "\n";
+        }
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return resreg;
+    }
+
+    public Integer visitParenFunc(FunctionsParser.ParenFuncContext ctx) {
+        return visit(ctx.func());
+    }
+
+    public Integer visitParenNum(FunctionsParser.ParenNumContext ctx) {
+        int resreg = ++lastRegister;
+        String instr = "%" + resreg + " = " + "add i32 " + ctx.NUM().getText() + ", 0\n";
+        args.add(resreg);
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return resreg;
+    }
+
+    public Integer visitArgvNum(FunctionsParser.ArgvNumContext ctx) {
+        int resreg = ++lastRegister;
+        String instr = "%" + resreg + " = " + "add i32 " + ctx.NUM().getText() + ", 0\n";
+        args.add(resreg);
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        return 1;
+    }
+
+    public Integer visitArgvId(FunctionsParser.ArgvIdContext ctx) {
+        String id = ctx.ID().getText();
+        int resreg = ++lastRegister;
+        String instr = "%" + resreg + " = ";
+        if (param.contains(id)) {
+            instr += "add i32 %" + id + ", 0\n";
+        }
+        if (var.contains(id)) {
+            instr += "load i32, i32* @" + id + "\n";
+        }
+        if (isCode) {
+            code += instr;
+        } else {
+            init += instr;
+        }
+        args.add(resreg);
+        return 1;
+    }
+
+    public Integer visitArgvFunc(FunctionsParser.ArgvFuncContext ctx) {
+        return visit(ctx.values(0)) + visit(ctx.values(1));
+    }
 }
